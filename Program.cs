ae7d1b304ba9f802fcf3d4a2f918a7042699b219
Daniel Nashokin <nashokin@hotmail.com>
2020-01-31 19:12:35 +1100

    Intial commit for demo BouncyCastle console app
    OpenPGP and RSA-OAEP demo console application to demostrate encrypting
    and decrypting these two crytography standards.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace BCCrypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("Enjoy cryptography!");
+        }
+
+        public static void Encrypt(string inputFile, string publicKey, string standard)
+        {
+            using (Stream input = File.OpenRead(inputFile), key = File.OpenRead(publicKey))
+            {
+                switch (standard)
+                {
+                    case "RSAES-OAEP":
+                        EncryptRsa(input, key, null);
+                        break;
+                    case "OpenPGP":
+                        EncryptPgp(input, key);
+                        break;
+                    default:
+                        Console.WriteLine("Encryption standard is not supported.");
+                        break;
+                };
+            }
+        }
+
+        public static void Decrypt(string inputFile, string privateKey, string standard)
+        {
+            using (Stream input = File.OpenRead(inputFile), key = File.OpenRead(privateKey))
+            {
+                switch (standard)
+                {
+                    case "RSAES-OAEP":
+                        DencryptRsa(input, key, null, null);
+                        break;
+                    case "OpenPGP":
+                        DencryptPgp(input, key, null);
+                        break;
+                    default:
+                        Console.WriteLine("Encryption standard is not supported.");
+                        break;
+                };
+            }
+        }
+
+        private static bool EncryptRsa(Stream input, Stream key, Stream hash)
+        {
+            return true;
+        }
+
+        private static bool DencryptRsa(Stream input, Stream key, Stream hash, char[] password)
+        {
+            return true;
+        }
+
+        private static bool EncryptPgp(Stream input, Stream key)
+        {
+            return true;
+        }
+
+        private static bool DencryptPgp(Stream input, Stream key, char[] password)
+        {
+            return true;
+        }
+    }
+}
---
+++ Standards.cs
@@ -0,0 +1,15 @@
+﻿namespace BCCrypto
+{
+    public static class Standards
+    {
+        /// <summary>
+        /// https://tools.ietf.org/html/rfc8017#page-19
+        /// </summary>
+        public static readonly string RsaOaep = "RSAES-OAEP";
+
+        /// <summary>
+        /// https://tools.ietf.org/html/rfc4880
+        /// </summary>
+        public static readonly string OpenPgp = "OpenPGP";
+    }
+}
